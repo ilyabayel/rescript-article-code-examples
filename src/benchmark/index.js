91286f4a801cab4a810ec754e462669c408b7e44
Ilya <i.kozlov.dev@gmail.com>
2022-05-22 19:11:17 +0600

    add sort suite + refactor

--- src/benchmark/index.js
+++ src/benchmark/index.js
@@ -1,69 +1,21 @@
 import Benchmark from "benchmark";
+import * as Belt_SortArray from "rescript/lib/es6/belt_SortArray.js";
+import * as Belt_Array from "rescript/lib/es6/belt_Array.js";
 
 const mapSuite = new Benchmark.Suite();
 const joinSuite = new Benchmark.Suite();
 const reduceSuite = new Benchmark.Suite();
 const filterSuite = new Benchmark.Suite();
+const sortSuite = new Benchmark.Suite();
 
 const arr = new Array(2000).fill(1).map((_, i) => i);
 const arr2 = new Array(2000).fill(1).map((_, i) => i);
-
-function mapU(a, f) {
-    var l = a.length;
-    var r = new Array(l);
-    for (var i = 0; i < l; ++i) {
-        r[i] = f(a[i]);
-    }
-    return r;
-}
-
-function joinWithU(a, sep, toString) {
-    var l = a.length;
-    if (l === 0) {
-        return "";
-    }
-    var lastIndex = l - 1 | 0;
-    var _i = 0;
-    var _res = "";
-    while (true) {
-        var res = _res;
-        var i = _i;
-        if (i === lastIndex) {
-            return res + toString(a[i]);
-        }
-        _res = res + (toString(a[i]) + sep);
-        _i = i + 1 | 0;
-        continue;
-    };
-}
-
-function reduceU(a, x, f) {
-    var r = x;
-    for (var i = 0, i_finish = a.length; i < i_finish; ++i) {
-        r = f(r, a[i]);
-    }
-    return r;
-}
-
-function keepU(a, f) {
-    var l = a.length;
-    var r = new Array(l);
-    var j = 0;
-    for (var i = 0; i < l; ++i) {
-        var v = a[i];
-        if (f(v)) {
-            r[j] = v;
-            j = j + 1 | 0;
-        }
-
-    }
-    r.length = j;
-    return r;
-}
+const arrSort = new Array(2000).fill(1).map((_, i) => i);
+const arrSort2 = new Array(2000).fill(1).map((_, i) => i);
 
 
 mapSuite
-    .add("belt map", () => mapU(arr, (x) => x * x))
+    .add("belt map", () => Belt_Array.mapU(arr, (x) => x * x))
     .add("js map", () => arr2.map((x) => x * x))
     .on('cycle', function (event) {
         console.log(String(event.target));
@@ -75,7 +27,7 @@ mapSuite
     .run({ 'async': true });
 
 joinSuite
-    .add("belt join", () => joinWithU(arr, "", s => s))
+    .add("belt join", () => Belt_Array.joinWithU(arr, "", s => s))
     .add("js join", () => arr2.join(""))
     .on('cycle', function (event) {
         console.log(String(event.target));
@@ -87,7 +39,7 @@ joinSuite
     .run({ 'async': true });
 
 reduceSuite
-    .add("belt reduce", () => reduceU(arr, 0, (a, b) => a + b))
+    .add("belt reduce", () => Belt_Array.reduceU(arr, 0, (a, b) => a + b))
     .add("js reduce", () => arr2.reduce((a, b) => a + b, 0))
     .on('cycle', function (event) {
         console.log(String(event.target));
@@ -99,8 +51,20 @@ reduceSuite
     .run({ 'async': true });
 
 filterSuite
-    .add("belt filter", () => keepU(arr, (x) => x >= 1000))
+    .add("belt filter", () => Belt_Array.keepU(arr, (x) => x >= 1000))
     .add("js filter", () => arr2.filter((x) => x >= 1000))
+    .on('cycle', function (event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function () {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+    })
+    // run async
+    .run({ 'async': true });
+
+sortSuite
+    .add("belt sort", () => Belt_SortArray.stableSortInPlaceByU(arrSort, (a, b) => b - a))
+    .add("js sort", () => arrSort2.sort((a, b) => b - a))
     .on('cycle', function (event) {
         console.log(String(event.target));
     })
